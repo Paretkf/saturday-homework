196d2d77caa635d599bfd347018c52cdb8f5177f
Rachata <paretk7@gmail.com>
2017-10-26 13:57:12 +0700

    แก้ชื่องงานครับ ที่ Home.vue
    ชื่องานผิดตอนเพื่อนแก้ conflict

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -21,13 +21,6 @@
           </td>
         </tr>
         <tr>
-          <td>5806021623065</td>
-          <td>สิริรัตน์ ปราชญ์พิริยะ</td>
-          <td>
-            <router-link to="/sirirat">car</router-link>
-            </td>
-            </tr>
-            <tr>
           <td>5806021633010</td>
           <td>ชลธิชา สูริผัส</td>
           <td>
@@ -80,7 +73,7 @@
           <td>5806021622051</td>
           <td>รชต ทองภักดี</td>
           <td>
-            <router-link to="/Rachata">Giphy Search</router-link>
+            <router-link to="/Rachata">Ip location</router-link>
           </td>
         </tr>
         <tr>
